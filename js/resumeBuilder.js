d1c205eb807512ce7577d11850f3d71baeb8c46f
futhrevo <k.rakeshlal@gmail.com>
2014-11-03 00:26:36 +0530

    Before Objects
    Day-2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Rakesh Kalyankar")
+var formattedName = HTMLheaderName.replace("%data%","Rakesh Kalyankar");
+var formattedRole = HTMLheaderRole.replace("%data%", "Development Engineer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
